de36e561ce44f6bf79717e14a17831629b0dabf4
Mauro Martinez <mmartinez@edrans.com>
2019-05-15 23:13:28 -0300

    Updates

---
+++ precache-manifest.e4a7bacdd513ea48ce6c25f91bf3ddcf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4642218ef387c551e27b",
+    "url": "/store/static/js/runtime~main.852bf9be.js"
+  },
+  {
+    "revision": "d6dd684d40c06cce40e4",
+    "url": "/store/static/js/main.1f9d5f5a.chunk.js"
+  },
+  {
+    "revision": "bf4b432bdaeb976ceff6",
+    "url": "/store/static/js/2.38e88c63.chunk.js"
+  },
+  {
+    "revision": "d6dd684d40c06cce40e4",
+    "url": "/store/static/css/main.6c6053ac.chunk.css"
+  },
+  {
+    "revision": "70012e8ecbffa1bdac0a99b8e78489c6",
+    "url": "/store/index.html"
+  }
+];
